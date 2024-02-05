176e8707bdaf469f656c0063a16f0c3113acd230
Nerd <72287783+AdrianBhp43@users.noreply.github.com>
2024-02-05 10:50:07 +0700

    first commit

---
+++ main/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func HelloHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprintf(w, dz.PostGeoIntersects("MONGOSTRING", "gisdz", "gisdz", r))
+}
+
+func handlerRequests() {
+	http.HandleFunc("/", HelloHTTP)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func main() {
+	handlerRequests()
+}
